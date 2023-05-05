d87defaa471afe2c1705f38a2be779228d481c76
Bernardo Gularte Kirsch <bernardogulartekirsch@gmail.com>
2023-05-05 00:52:40 -0300

    config do banco, CRUD e itens dinâmicos

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from galeria.models import Fotografia
+
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda')
+    list_display_links = ('id', 'nome', 'legenda')
+    search_fields = ('nome',)
+
+admin.site.register(Fotografia, ListandoFotografias)
